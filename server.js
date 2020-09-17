3704390703aa8eaad682044814ea4f3b85d61842
Chris Roschi <chrisroschi53@gmail.com>
2020-09-17 11:42:18 +0930

    attempting to fix heroku error

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/transaction',
+  process.env.MONGODB_URI || 'mongodb://localhost/budget',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -28,6 +28,7 @@ mongoose.connect(
 
 // routes
 app.use(require("./routes/api.js"));
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
